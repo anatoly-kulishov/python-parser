c0c9925c361cdd211f57b77e463cb6d8cd305a03
RewQ <rewq_gol_47@mail.ru>
2021-02-14 18:44:54 +0300

    'start'

--- main.py
+++ main.py
@@ -18,10 +18,12 @@ class Parser:
         print(self.get_url, '\n * Status Request:', str(status_request))
 
     @staticmethod
-    def write_txt(name_txt, title, price):
+    def write_txt(name_txt, title, price, availability, json):
         file = open(name_txt, 'a', encoding='UTF-8')
         file.write('Title: ' + title + '\n')
-        file.write('Price: ' + price + '\n\n')
+        file.write('Price: ' + price + '\n')
+        file.write('Availability: ' + availability + '\n')
+        file.write('JSON: ' + json + '\n\n')
         file.close()
 
 
@@ -38,6 +40,9 @@ class ParseProducts(Parser):
             self.pages.append(requests.get(str(self.get_url)))  # str(id_page))
             self.status_request = requests.get(self.r_url)
 
+    def create_single_page_requests(self):
+        pass
+
     def get_status_request(self):
         if self.status_request.status_code == 200:
             status_request = True
@@ -50,16 +55,21 @@ class ParseProducts(Parser):
         for r in self.pages:
             html = BS(r.content, 'html.parser')
             text_404 = 'None'
-            title_text, price_text = text_404, text_404
+            title_text, price_text, availability_text = text_404, text_404, text_404
 
             for el in html.select('.catalog-section-item'):
+                attributes_dictionary = el.attrs
+                json = attributes_dictionary['data-data']
+
                 title = el.select('.catalog-section-item-name-wrapper')
                 price = el.select('div.catalog-section-item-price-discount span')
+                availability = el.select('.catalog-section-item-quantity')
 
                 title_text = title[0].text if title else text_404
                 price_text = price[0].text if price else text_404
+                availability_text = availability[0].text if availability else text_404
 
-                Parser.write_txt('products.txt', title_text, price_text)
+                Parser.write_txt('products.txt', title_text, price_text, availability_text, json)
 
 
 # my_site = Parser('http://absolute-nature-products.develop-web.site')
@@ -68,5 +78,7 @@ class ParseProducts(Parser):
 
 my_site_products_data = ParseProducts('https://greatsteve.ru/catalog/appleiphone/', 1)
 my_site_products_data.create_requests()
-my_site_products_data.get_status_request()
-my_site_products_data.get_data()
+my_site_products_data.create_single_page_requests()
+
+# my_site_products_data.get_status_request()
+# my_site_products_data.get_data()
