cb5e4cd02e9d5e9577850e25443502f5a2ad0452
dogram99 <rewq_gol_47@mail.ru>
2021-02-15 10:08:30 +0300

    'start'

--- main.py
+++ main.py
@@ -18,12 +18,10 @@ class Parser:
         print(self.get_url, '\n * Status Request:', str(status_request))
 
     @staticmethod
-    def write_txt(name_txt, title, price, availability, json):
+    def write_txt(name_txt, title, price):
         file = open(name_txt, 'a', encoding='UTF-8')
         file.write('Title: ' + title + '\n')
-        file.write('Price: ' + price + '\n')
-        file.write('Availability: ' + availability + '\n')
-        file.write('JSON: ' + json + '\n\n')
+        file.write('Price: ' + price + '\n\n')
         file.close()
 
 
@@ -34,14 +32,15 @@ class ParseProducts(Parser):
         self.max_page = max_page
         self.r_url = None
 
-    def create_requests(self):
+    def create_request_for_several_pages(self):
         for id_page in range(1, self.max_page + 1):
             self.r_url = str(self.get_url) + str(id_page)
-            self.pages.append(requests.get(str(self.get_url)))  # str(id_page))
+            self.pages.append(requests.get(str(self.get_url) + str(id_page)))
             self.status_request = requests.get(self.r_url)
 
+    @staticmethod
     def create_single_page_requests(self):
-        pass
+        print('create_single_page_requests')
 
     def get_status_request(self):
         if self.status_request.status_code == 200:
@@ -55,30 +54,23 @@ class ParseProducts(Parser):
         for r in self.pages:
             html = BS(r.content, 'html.parser')
             text_404 = 'None'
-            title_text, price_text, availability_text = text_404, text_404, text_404
+            title_text, price_text = text_404, text_404
 
-            for el in html.select('.catalog-section-item'):
-                attributes_dictionary = el.attrs
-                json = attributes_dictionary['data-data']
-
-                title = el.select('.catalog-section-item-name-wrapper')
-                price = el.select('div.catalog-section-item-price-discount span')
-                availability = el.select('.catalog-section-item-quantity')
+            for el in html.select('li.product'):
+                title = el.select('.woocommerce-loop-product__title')
+                price = el.select('.price')
 
                 title_text = title[0].text if title else text_404
                 price_text = price[0].text if price else text_404
-                availability_text = availability[0].text if availability else text_404
 
-                Parser.write_txt('products.txt', title_text, price_text, availability_text, json)
+                Parser.write_txt('products.txt', title_text, price_text)
 
 
 # my_site = Parser('http://absolute-nature-products.develop-web.site')
 # my_site.create_requests()
 # my_site.get_status_request()
 
-my_site_products_data = ParseProducts('https://greatsteve.ru/catalog/appleiphone/', 1)
-my_site_products_data.create_requests()
-my_site_products_data.create_single_page_requests()
-
-# my_site_products_data.get_status_request()
-# my_site_products_data.get_data()
+my_site_products_data = ParseProducts('http://absolute-nature-products.develop-web.site/shop/page/', 2)
+my_site_products_data.create_request_for_several_pages()
+my_site_products_data.get_status_request()
+my_site_products_data.get_data()
